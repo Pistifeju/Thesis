9792b5db110b1e7e0db612a2d33abd5d4174149a
Pistifeju <juhasz.pisti7253@gmail.com>
2022-11-13 15:34:56 +0100

    Add MainViewController

---
+++ Thesis/Controllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Thesis
+//
+//  Created by István Juhász on 2022. 11. 12..
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Selectors
+    
+}
